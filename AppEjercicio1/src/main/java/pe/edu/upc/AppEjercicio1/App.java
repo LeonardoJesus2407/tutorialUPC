935d222073e1c2167ad9bb7e2a0653f7da02f6cb
LeonardoChuchon+ <leojesus_2407@hotmail.com>
2016-08-16 09:28:48 -0500

    modifique parte 1

---
+++ AppEjercicio1/src/main/java/pe/edu/upc/AppEjercicio1/App.java
@@ -0,0 +1,34 @@
+package pe.edu.upc.AppEjercicio1;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Scanner s=new Scanner(System.in);
+    	int numero;
+    	double Numero;
+    	//Wrapper (Clases emvolventes) ==>Casting
+    	Double valores = new Double (13);
+    	
+    	String[] arrNombres=new String [3];
+    	
+    	
+        System.out.println( "Hello World!" );
+    
+        for (int i = 0; i<arrNombres.length; i++ ){
+        	
+        	System.out.println( "Nombre" + (i+1) );
+        	arrNombres[i]=s.nextLine();
+        	
+        	
+        }
+    
+    
+    }
+}
